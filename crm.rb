aeebcb5b8cb994e933ed4b8fcb15565e6ad2c478
Margarita Bartholomew <margarita.veracruz@gmail.com>
2017-04-02 23:07:18 -0400

    update contact method

--- crm.rb
+++ crm.rb
@@ -1,3 +1,5 @@
+require_relative 'contact.rb'
+
 class CRM
 
   def initialize
@@ -33,10 +35,41 @@ class CRM
   end
 
   def add_new_contact
+    print 'Enter First Name:'
+    first_name = gets.chomp
+
+    print 'Enter Last Name:'
+    last_name = gets.chomp
+
+    print 'Enter Email Address:'
+    email = gets.chomp
+
+    print 'Enter a note:'
+    note = gets.chomp
+
+    Contact.create(first_name, last_name, email, note)
 
   end
 
   def modify_existing_contact
+    print 'Enter contact ID to modify:'
+    id = gets.to_i
+
+    contact_to_modify = Contact.find(id)
+
+    puts 'Please select the attribute to modify'
+    puts '[first_name] to update the first name'
+    puts '[last-name] to update the last name'
+    puts '[email] to update the e-mail address'
+    puts '[note] to update the note'
+
+    attribute_to_modify = gets.chomp
+
+    puts "What would you like to change the #{attribute_to_modify} to?"
+
+    new_value = gets.chomp
+
+    contact_to_modify.update("#{attribute_to_modify}, #{new_value}")
 
   end
 
