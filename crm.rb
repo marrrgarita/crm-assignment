fe487ecc14497268b5aa4b38f3703b16bff94d1d
Margarita Bartholomew <margarita.veracruz@gmail.com>
2017-04-02 22:41:08 -0400

    main menu options

--- crm.rb
+++ crm.rb
@@ -5,11 +5,20 @@ class CRM
   end
 
   def main_menu
-
+    while true
+      print_main_menu
+      user_selected = gets.to_i
+      call_option(user_selected)
+    end
   end
 
   def print_main_menu
-
+    puts '[1] Add a new contact'
+    puts '[2] Modify an existing contact'
+    puts '[3] Delete a contact'
+    puts '[4] Display all the contacts'
+    puts '[5] Search by attribute'
+    puts '[6] Exit'
   end
 
   def call_option
@@ -29,11 +38,11 @@ class CRM
   end
 
   def display_all_contacts
-    
+
   end
 
   def search_by_attribute
-    
+
   end
 
 
